aabadb94820d07bfcbf736afeaeda46c71885957
cndcomp <gamerboyfury@gmail.com>
2026-05-10 10:28:49 +1000

    Add vite config and index.html in root

--- Desktop/spark-app/vite.config.js
+++ Desktop/spark-app/vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+  }
 })
